458e06f845b221dc11439e188b7e429012f74ee6
jtambo99 <jtambo@live.com>
2019-11-01 21:14:28 +0100

    ahora puede buscar

---
+++ first-servlet/src/baseDatos/Vehiculo.java
@@ -0,0 +1,198 @@
+package baseDatos;
+/**
+ * Clase para representar un vehículo de la BD, debe estar provisto de: - id -
+ * tipo (puede ser: Camion, Moto, TransportePublico, Coche) - segment (segmento
+ * comercial) - euro_star (clasificacion ambiental en base a la contaminación
+ * del vehículo) - engine_type (tecnologia del motor) - fuel (tipo de
+ * combustible) - pollutant (tipo de emision) - emission_factor (factor de
+ * emision del compuesto <pollutant>)
+ * 
+ * @author Pedro Tamargo Allue
+ */
+public class Vehiculo {
+    private int id;
+    private String tipo, euro_star, fuel, engine_type, segment, pollutant;
+    private float emission_factor;
+
+    /**
+     * Constructor de la clase vehiculo
+     * 
+     * @param id              identificador del vehiculo
+     * @param tipo            tipo del vehiculo
+     * @param segment         segmento comercial del vehiculo
+     * @param euro_star       clasificacion ambiental del vehiculo
+     * @param engine_type     tipo del motor (NULL si no se conoce)
+     * @param fuel            tipo de combustible del vehiculo
+     * @param pollutant       compuesto contaminante del vehiculo
+     * @param emission_factor factor de emision del contaminante <pollutant> del
+     *                        vehiculo
+     */
+    public Vehiculo(int id, String tipo, String segment, String euro_star, String engine_type, String fuel,
+            String pollutant, float emission_factor) {
+        this.id = id;
+        this.tipo = tipo;
+        this.segment = segment;
+        this.euro_star = euro_star;
+        this.engine_type = engine_type;
+        this.fuel = fuel;
+        this.pollutant = pollutant;
+        this.emission_factor = emission_factor;
+    }
+
+    /**
+     * Constructor copia del vehiculo
+     * 
+     * @param orig vehiculo a copiar
+     */
+    public Vehiculo(Vehiculo orig) {
+        this(orig.id, orig.tipo, orig.segment, orig.euro_star, orig.engine_type, orig.fuel, orig.pollutant,
+                orig.emission_factor);
+    }
+
+    // Metodos setter y getter
+
+    /**
+     * Getter del identificador del vehiculo
+     * 
+     * @return identificador del vehiculo
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Setter del identificador del vehiculo
+     * 
+     * @param id identificador actualizado
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Getter del tipo de vehiculo
+     * 
+     * @return tipo de vehiculo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * Setter del tipo de vehiculo
+     * 
+     * @param tipo tipo actualizado
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * Getter de la clasificacion ambiental del vehiculo
+     * 
+     * @return clasificacion ambiental del vehiculo
+     */
+    public String getEuro_star() {
+        return euro_star;
+    }
+
+    /**
+     * Setter de la clasificacion ambiental del vehiculo
+     * 
+     * @param euro_star clasificacion ambiental actualizada
+     */
+    public void setEuro_star(String euro_star) {
+        this.euro_star = euro_star;
+    }
+
+    /**
+     * Getter del tipo de combustible del vehiculo
+     * 
+     * @return tipo de combustible del vehiculo
+     */
+    public String getFuel() {
+        return fuel;
+    }
+
+    /**
+     * Setter del tipo de combustible del vehiculo
+     * 
+     * @param fuel tipo de combustible actualizado
+     */
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    /**
+     * Getter de la tecnología del motor del vehiculo
+     * 
+     * @return tecnologia del motor
+     */
+    public String getEngine_type() {
+        return engine_type;
+    }
+
+    /**
+     * Setter de la tecnologia del motor del vehiculo
+     * 
+     * @param engine_type tecnologia actualizada
+     */
+    public void setEngine_type(String engine_type) {
+        this.engine_type = engine_type;
+    }
+
+    /**
+     * Getter del segmento comercial del vehiculo
+     * 
+     * @return segmento del vehiculo
+     */
+    public String getSegment() {
+        return segment;
+    }
+
+    /**
+     * Setter del segmento comercial del vehiculo
+     * 
+     * @param segment segmento actualizado
+     */
+    public void setSegment(String segment) {
+        this.segment = segment;
+    }
+
+    /**
+     * Getter del compuesto contaminante del vehiculo
+     * 
+     * @return compuesto contaminante del vehiculo
+     */
+    public String getPollutant() {
+        return pollutant;
+    }
+
+    /**
+     * Setter del compuesto contaminante del vehiculo
+     * 
+     * @param pollutant compuesto contaminante actualizado
+     */
+    public void setPollutant(String pollutant) {
+        this.pollutant = pollutant;
+    }
+
+    /**
+     * Getter del factor de emision de <pollutant> del vehiculo
+     * 
+     * @return factor de emision del vehiculo
+     */
+    public float getEmission_factor() {
+        return emission_factor;
+    }
+
+    /**
+     * Setter del factor de emision de <pollutant> del vehiculo
+     * 
+     * @param emission_factor factor de emision actualizado
+     */
+    public void setEmission_factor(float emission_factor) {
+        this.emission_factor = emission_factor;
+    }
+
+}
---
+++ first-servlet/src/baseDatos/VehiculoDAO.java
@@ -0,0 +1,119 @@
+package baseDatos;
+/**
+ * Clase DAO para la clase vehiculo. Permitira hacer comprobaciones de factores
+ * de emision sobre distintos parametros.
+ * 
+ * @author Pedro Tamargo Allue
+ */
+
+import java.sql.*;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class VehiculoDAO {
+
+    protected static final String findALL = "select * from Vehiculo";
+    // protected static final String findOverCondition_flat = "select * from
+    // Vehiculo where ";
+    protected static final String findOverCondition_tipo = "select * from Vehiculo where tipo = ? ",
+            findOverCondition_segment = "select * from Vehiculo where segment = ? ",
+            findOverCondition_euro_star = "select * from Vehiculo where euro_star = ? ",
+            findOverCondition_engine_type = "select * from Vehiculo where engine_type = ? ",
+            findOverCondition_fuel = "select * from Vehiculo where fuel = ? ",
+            findOverCondition_pollutant = "select * from Vehiculo where pollutant = ? ";
+
+    /**
+     * Metodo que devuelve una lista con todos los elementos de la tabla Vehiculo
+     * 
+     * @return lista de vehiculos que contiene todos los elementos de la tabla
+     *         Vehiculo de la BD
+     */
+    public static List<Vehiculo> findAllVehiculos() {
+        List<Vehiculo> lista = new LinkedList<>();
+        try {
+            Connection c = ConnectionManager.getConnection();
+            PreparedStatement stmnt = c.prepareStatement(findALL);
+            ResultSet rs = stmnt.executeQuery();
+            while (rs.next()) {
+                lista.add(new Vehiculo(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5),
+                        rs.getString(6), rs.getString(7), rs.getFloat(8)));
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return lista;
+    }
+
+    /**
+     * Metodo que devuelve una lista con todos los elementos de la tabla vehiculo
+     * que cumplen la condicion de que sus campos son iguales a los de los
+     * parametros pasados si estos son distintos de null.
+     * 
+     * @param tipo        tipo del vehiculo
+     * @param segment     segmento comercial del vehiculo
+     * @param euro_star   clasificacion ambiental del vehiculo
+     * @param engine_type tecnologia del motor del vehiculo
+     * @param fuel        tipo de combustible del vehiculo
+     * @param pollutant   compuesto contaminante del vehiculo
+     * @return lista con los vehiculos que cumplen las caracteristicas descritas.
+     */
+    public static List<Vehiculo> findOverCondition(String tipo, String segment, String euro_star, String engine_type,
+            String fuel, String pollutant) {
+        final String intersect = "intersect ";
+        List<Vehiculo> lista = new LinkedList<>();
+        int index = 1;
+        try {
+            Connection c = ConnectionManager.getConnection();
+            String sqlStmnt = "";
+            Map<Integer, String> mapa = new TreeMap<>();
+            if (tipo != null) {
+                sqlStmnt = (index == 1 ? findOverCondition_tipo : intersect + findOverCondition_tipo);
+                mapa.put(index, tipo);
+                index++;
+            }
+            if (segment != null) {
+                sqlStmnt = (index == 1 ? findOverCondition_segment : intersect + findOverCondition_segment);
+                mapa.put(index, segment);
+                index++;
+            }
+            if (euro_star != null) {
+                sqlStmnt = (index == 1 ? findOverCondition_euro_star : intersect + findOverCondition_euro_star);
+                mapa.put(index, euro_star);
+                index++;
+            }
+            if (engine_type != null) {
+                sqlStmnt = (index == 1 ? findOverCondition_engine_type : intersect + findOverCondition_engine_type);
+                mapa.put(index, engine_type);
+                index++;
+            }
+            if (fuel != null) {
+                sqlStmnt = (index == 1 ? findOverCondition_fuel : intersect + findOverCondition_fuel);
+                mapa.put(index, fuel);
+                index++;
+            }
+            if (pollutant != null) {
+                sqlStmnt = (index == 1 ? findOverCondition_pollutant : intersect + findOverCondition_pollutant);
+                mapa.put(index, pollutant);
+                index++;
+            }
+            PreparedStatement ps = c.prepareStatement(sqlStmnt);
+            // Ahora recorremos el map para saber los valores de los indices a aplicar
+            for (int i = 1; i <= 6 && mapa.containsKey(i); i++) {
+                String value = mapa.get(i);
+                ps.setString(i, value);
+            }
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                lista.add(new Vehiculo(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5),
+                        rs.getString(6), rs.getString(7), rs.getFloat(8)));
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return lista;
+    }
+}
