e18bf563132452acad01cc22a5153f25653142a6
jtambo99 <jtambo@live.com>
2019-11-04 12:42:52 +0100

    Añadido mapa de calor

---
+++ first-servlet/src/servlets/FirstServlet.java
@@ -0,0 +1,25 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+ 
+
+public class FirstServlet extends HttpServlet{
+
+ @Override
+ protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+                  throws ServletException, IOException { 
+            PrintWriter out = resp.getWriter();
+            out.println("<html>");
+            out.println("<body>");
+            out.println("hoy es " + new Date());
+            out.println("</body>");
+            out.println("</html>");
+ }
+}
---
+++ first-servlet/src/servlets/RegisterServlet.java
@@ -0,0 +1,45 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import baseDatos.*;
+
+public class RegisterServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String nickName = req.getParameter("nickName");
+		String nombre = req.getParameter("nombre");
+		String apellidos = req.getParameter("apellidos");
+		String pass1= req.getParameter("password");
+		String pass2= req.getParameter("password1");
+		String email = req.getParameter("email");
+		String vehic = req.getParameter("vehiculo");
+		
+		Usuario introducido = new Usuario(nickName, pass1, nombre,apellidos, email, 0);
+		if (UsuarioDAO.validateUser(introducido)) {
+			//Faltaria especificar que hace si ha sido correcto el login
+			response(resp, "login ok");
+		} else {
+			response(resp, "invalid login");
+		}
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+
+	private void response(HttpServletResponse resp, String msg) throws IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<t1>" + msg + "</t1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
