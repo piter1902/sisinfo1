7382a94f6262bc751a36b64ac4360400e5270221
Pedro Tamargo <pedro.tamargo.allue@protonmail.com>
2019-11-15 13:30:37 +0100

    Para modificar los tipos de los Vehiculos de la BD

---
+++ Practica2/sql/modificar_tipos.sql
@@ -0,0 +1,5 @@
+UPDATE Vehiculo set tipo = 'Coche' where tipo = 'passenger_car';
+UPDATE Vehiculo set tipo = 'Camion' where tipo = 'heavy_duty_truck';     
+UPDATE Vehiculo set tipo = 'Camion' where tipo = 'light_commercial_vehicle';     
+UPDATE Vehiculo set tipo = 'Bus' where tipo = 'bus';
+UPDATE Vehiculo set tipo = 'Moto' where tipo = 'l-category';
