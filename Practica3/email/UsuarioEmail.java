1ef4f4d138874553ed81d9a531bc4a002e989346
Pedro Tamargo <pedro.tamargo.allue@protonmail.com>
2019-11-20 09:46:45 +0100

    Cambios para email

--- Practica3/email/UsuarioEmail.java
+++ Practica3/email/UsuarioEmail.java
@@ -32,6 +32,47 @@ public class UsuarioEmail {
 	}
 
 	/**
+	 * Metodo que envia un email al usuario, si email de <old> == <nuevo>, solo lo
+	 * enviará una vez, en caso contrario, lo enviará a ambos para confirmar los
+	 * cambios.
+	 * 
+	 * @param old   parametros del Usuario antes del cambio. Tiene que ser completo.
+	 * @param nuevo parametros del Usuario tras los cambios. Tiene que contener, al
+	 *              menos, el login del nuevo usuario.
+	 * @return true si todo ha ido bien
+	 */
+	public static boolean sendChangesEmail(Usuario old, Usuario nuevo) {
+		boolean ok = false;
+		Usuario nuevo1 = UsuarioDAO.findUserByLogin(nuevo.getLogin());
+		if (nuevo1 != null) {
+			String asunto = "Cambios en la cuenta asociada a " + nuevo.getLogin();
+			String cuerpo = generateUserChangesMail(old, nuevo1);
+			if (old.getEmail().equals(nuevo.getEmail())) {
+				// Los mails son iguales, solamente mandamos 1
+				ok = EmailAdapter.sendEmail(old.getEmail(), asunto, cuerpo);
+			} else {
+				// No coinciden, enviamos 2 correos
+				ok = EmailAdapter.sendEmail(old.getEmail(), asunto, cuerpo)
+						&& EmailAdapter.sendEmail(nuevo1.getEmail(), asunto, cuerpo);
+			}
+		}
+		return ok;
+	}
+
+	/**
+	 * Metodo que genera el cuerpo de un correo electronico en el cual aparecen los
+	 * cambios de una cuenta de Usuario.
+	 * 
+	 * @param old   parametros del usuario antiguos. Debe ser completo.
+	 * @param nuevo parametros del nuevo usuario. Debe ser completo
+	 * @return cuerpo del correo electronico.
+	 */
+	private static String generateUserChangesMail(Usuario old, Usuario nuevo) {
+		String cuerpo = "";
+		return cuerpo;
+	}
+
+	/**
 	 * Metodo que genera un mensaje de bienvenida para ser enviado al usuario <user>
 	 * 
 	 * @param user usuario para el cual se personalizará el mensaje
