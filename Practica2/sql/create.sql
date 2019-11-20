b13f6b9c2dd72d537f7e13dbb079018e1e5a92e8
Pedro Tamargo <pedro.tamargo.allue@protonmail.com>
2019-11-20 09:44:39 +0100

    Cambio en el create

--- Practica2/sql/create.sql
+++ Practica2/sql/create.sql
@@ -17,7 +17,7 @@ CREATE TABLE Comentario
 (
     comentarioId BIGINT NOT NULL AUTO_INCREMENT,
     texto VARCHAR(250) NOT NULL,
-    email varchar(20) not null,
+    email varchar(30) not null,
     nombre VARCHAR(20) not null,
     apellidos varchar(30),
     antecesor bigint,
@@ -53,3 +53,6 @@ CREATE TABLE Consulta
 alter table Vehiculo AUTO_INCREMENT = 1;
 alter table Comentario AUTO_INCREMENT = 1;
 alter table Consulta AUTO_INCREMENT = 1;
+
+-- Insercion para que comentario funcione
+insert into Comentario(comentarioId, texto, email, nombre, apellidos, antecesor) values (1, "Esto solo es referenciado por los mensajes de Usuarios", "ecobicizara@gmail.com", "ecobicizara", "ecobicizara", NULL);
