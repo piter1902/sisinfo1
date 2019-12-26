b5210c68b13a47fc1f0806891be876b00ff1c3cf
Pedro Tamargo <pedro.tamargo.allue@protonmail.com>
2019-12-26 18:32:12 +0100

    Codigo formateado

--- Practica5/PruebaLucene/src/sistemasinformacion/practica5/IndexadorYBuscador.java
+++ Practica5/PruebaLucene/src/sistemasinformacion/practica5/IndexadorYBuscador.java
@@ -21,7 +21,6 @@ import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.io.IOException;
@@ -32,176 +31,162 @@ import java.io.InputStreamReader;
 import java.nio.file.Paths;
 import java.io.BufferedReader;
 
-
 /**
  * Clase de ejemplo de un indexador y buscador usando Lucene
+ * 
  * @author sisinf
  *
  */
-public class IndexadorYBuscador{
+public class IndexadorYBuscador {
 
 	/**
 	 * Relación de ficheros a indexar / buscar
 	 */
-	private Collection <String> ficherosAIndexar = new ArrayList<String>();
+	private Collection<String> ficherosAIndexar = new ArrayList<String>();
 	/**
 	 * Relación de palabras clave a buscar
 	 */
-	private Collection <String> queries = new ArrayList <String>();
+	private Collection<String> queries = new ArrayList<String>();
 	/**
-	 * Analizar utilizado por el indexador / buscador 
+	 * Analizar utilizado por el indexador / buscador
 	 */
 	private Analyzer analizador;
-	
+
 	private final static String INDEXDIR = "./ficheros/indice";
-	
-	
 
 	/**
 	 * Constructor parametrizado
+	 * 
 	 * @param ficherosAIndexar Colección de ficheros a indexar
-	 * @param queries Colección de palabras a buscar
+	 * @param queries          Colección de palabras a buscar
 	 */
-	public IndexadorYBuscador(Collection<String> ficherosAIndexar, Collection<String> queries){
+	public IndexadorYBuscador(Collection<String> ficherosAIndexar, Collection<String> queries) {
 		this.ficherosAIndexar = ficherosAIndexar;
 		this.queries = queries;
-		
+
 		analizador = new SimpleAnalyzer();
 
-/*		try {
-			FileReader reader = new FileReader("./ficheros/stopwords.txt");
-			analizador = new StandardAnalyzer(reader);
-		} catch (Exception e) {
-			System.out.println("Error leyendo fichero de Stop Words. Usando valor por defecto");
-			analizador = new StandardAnalyzer();
-		} 
-*/
-		//analizador = new SpanishAnalyzer();
+		/*
+		 * try { FileReader reader = new FileReader("./ficheros/stopwords.txt");
+		 * analizador = new StandardAnalyzer(reader); } catch (Exception e) {
+		 * System.out.
+		 * println("Error leyendo fichero de Stop Words. Usando valor por defecto");
+		 * analizador = new StandardAnalyzer(); }
+		 */
+		// analizador = new SpanishAnalyzer();
 
-	
 	}
-	
-	
-	
+
 	/**
 	 * Añade un fichero al índice
+	 * 
 	 * @param indice Indice que estamos construyendo
-	 * @param path ruta del fichero a indexar
+	 * @param path   ruta del fichero a indexar
 	 * @throws IOException
 	 */
-	private void anhadirFichero(IndexWriter indice, String path) 
-	throws IOException {
+	private void anhadirFichero(IndexWriter indice, String path) throws IOException {
 		InputStream inputStream = new FileInputStream(path);
-		BufferedReader inputStreamReader = new BufferedReader(
-				new InputStreamReader(inputStream,"UTF-8"));
-		
-		Document doc = new Document();   
+		BufferedReader inputStreamReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+
+		Document doc = new Document();
 		doc.add(new TextField("contenido", inputStreamReader));
 		doc.add(new StringField("path", path, Field.Store.YES));
 		indice.addDocument(doc);
 	}
-	
-	
-	
+
 	/**
 	 * Indexa los ficheros incluidos en "ficherosAIndexar"
+	 * 
 	 * @return un índice (Directory) en memoria, con los índices de los ficheros
 	 * @throws IOException
 	 */
-	private Directory crearIndiceEnUnDirectorio() throws IOException{
+	private Directory crearIndiceEnUnDirectorio() throws IOException {
 		IndexWriter indice = null;
 		Directory directorioAlmacenarIndice = new MMapDirectory(Paths.get(INDEXDIR));
 
 		IndexWriterConfig configuracionIndice = new IndexWriterConfig(analizador);
 
 		indice = new IndexWriter(directorioAlmacenarIndice, configuracionIndice);
-		
+
 		for (String fichero : ficherosAIndexar) {
 			anhadirFichero(indice, fichero);
 		}
-		
+
 		indice.close();
 		return directorioAlmacenarIndice;
 	}
-	
-	
-	
+
 	/**
 	 * Busca la palabra indicada en queryAsString en el directorioDelIndice.
+	 * 
 	 * @param directorioDelIndice
 	 * @param paginas
 	 * @param hitsPorPagina
 	 * @param queryAsString
 	 * @throws IOException
 	 */
-	private void buscarQueryEnIndice(Directory directorioDelIndice, 
-										int paginas, 
-										int hitsPorPagina, 
-										String queryAsString)
-	throws IOException{
+	private void buscarQueryEnIndice(Directory directorioDelIndice, int paginas, int hitsPorPagina,
+			String queryAsString) throws IOException {
 
 		DirectoryReader directoryReader = DirectoryReader.open(directorioDelIndice);
 		IndexSearcher buscador = new IndexSearcher(directoryReader);
-		
-		QueryParser queryParser = new QueryParser("contenido", analizador); 
+
+		QueryParser queryParser = new QueryParser("contenido", analizador);
 		Query query = null;
-		try{
+		try {
 			query = queryParser.parse(queryAsString);
 			TopDocs resultado = buscador.search(query, paginas * hitsPorPagina);
 			ScoreDoc[] hits = resultado.scoreDocs;
-		      
+
 			System.out.println("\nBuscando " + queryAsString + ": Encontrados " + hits.length + " hits.");
 			int i = 0;
-			for (ScoreDoc hit: hits) {
+			for (ScoreDoc hit : hits) {
 				int docId = hit.doc;
-				
+
 				Document doc = buscador.doc(docId);
 				System.out.println((++i) + ". " + doc.get("path") + "\t" + hit.score);
 			}
 
-		}catch (ParseException e){
+		} catch (ParseException e) {
 			throw new IOException(e);
-		}	
+		}
 	}
-	
-	
-	
+
 	/**
-	 * Ejecuta en el índice una búsqueda por cada una de las palabras clave solicitadas. <p>
-	 * Las palabras clave solicitadas están en la propiedad global "queries". 
+	 * Ejecuta en el índice una búsqueda por cada una de las palabras clave
+	 * solicitadas.
+	 * <p>
+	 * Las palabras clave solicitadas están en la propiedad global "queries".
+	 * 
 	 * @param directorioDelIndice índice
-	 * @param paginas 
+	 * @param paginas
 	 * @param hitsPorPagina
 	 * @throws IOException
 	 */
-	private void buscarQueries(Directory directorioDelIndice, int paginas, int hitsPorPagina)
-	throws IOException{
+	private void buscarQueries(Directory directorioDelIndice, int paginas, int hitsPorPagina) throws IOException {
 		for (String palabra : queries) {
-			buscarQueryEnIndice(directorioDelIndice, 
-								paginas, 
-								hitsPorPagina, 
-								palabra);			
+			buscarQueryEnIndice(directorioDelIndice, paginas, hitsPorPagina, palabra);
 		}
 	}
-	
-	
-	
+
 	/**
 	 * Programa principal de prueba. Rellena las colecciones "ficheros" y "queries"
+	 * 
 	 * @param args
 	 * @throws IOException
 	 */
-	public static void main(String[]args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		// Establecemos la lista de ficheros a indexar
-		Collection <String> ficheros = new ArrayList <String>();
+		Collection<String> ficheros = new ArrayList<String>();
 		ficheros.add("./ficheros/uno.txt");
 		ficheros.add("./ficheros/dos.txt");
 		ficheros.add("./ficheros/tres.txt");
 		ficheros.add("./ficheros/cuatro.txt");
 
 		// Establecemos las palabras clave a utilizar en la búsqueda
-		Collection <String> queries = new ArrayList <String>();
+		Collection<String> queries = new ArrayList<String>();
 		queries.add("Contaminación");
+//		queries.add("Contaminacion");
 		queries.add("cambio climatico");
 		queries.add("cambio climático");
 		queries.add("cambio");
@@ -214,14 +199,13 @@ public class IndexadorYBuscador{
 
 		// Indexamos los ficheros
 		Directory directorioDelIndiceCreado = ejemplo.crearIndiceEnUnDirectorio();
-		
+
 		// Abrimos un ficher indexado previamente
-		//Directory directorioDelIndiceCreado = MMapDirectory.open(Paths.get(INDEXDIR));
-		
+		// Directory directorioDelIndiceCreado =
+		// MMapDirectory.open(Paths.get(INDEXDIR));
+
 		// Ejecutamos la búsqueda de las palabras clave
 		ejemplo.buscarQueries(directorioDelIndiceCreado, ficheros.size(), 1);
 	}
-	
+
 }
-
-
